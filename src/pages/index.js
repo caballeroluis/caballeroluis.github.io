cd477ebaae0626b0456e66cadb5f142673545f4b
Luis Caballero <111797757+caballeroluis@users.noreply.github.com>
2023-09-28 19:23:48 +0200

    Update Seo title

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ import {
 export default function IndexPage() {
   return (
     <>
-      <Seo title="Gatsby Starter for Portfolio Minimal" />
+      <Seo title="Luis Caballero - Portfolio" />
       <Page useSplashScreenAnimation>
         <HeroSection sectionId="hero" />
         <AboutSection sectionId="about" heading="About me" />
